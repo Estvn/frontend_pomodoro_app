88b9213f9a942e5ac80ae19befb90164909f7dc3
uayebcaballero <uayeb.caballero@gmail.com>
2025-11-04 21:14:50 -0600

    release scripts

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000',
+    baseURL: process.env.EXPO_PUBLIC_API_URL || "https://unah-pomodoro-api.azurewebsites.net",
+    //baseURL: process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000",
     timeout: 5000,
 });
 
